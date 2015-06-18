b839c7187ae51dbf925a585b6b312f8740b828ed
Eric Katz <ericpkatz@gmail.com>
2015-06-18 01:00:03 +0000

    some refactoring

--- fizz-buzz.js
+++ fizz-buzz.js
@@ -1,40 +1,6 @@
 module.exports = {
-    generate: function(startAt, endAt){
-        if(!endAt){
-            endAt = startAt;
-            startAt = 1;
-        }
-       var results = []; 
-       for(var i = startAt; i <= endAt; i++){
-           if(isFizzBuzz(i))
-              results.push("FizzBuzz");
-           else if(isFizz(i))
-                results.push("Fizz");
-            else if(isBuzz(i))
-                results.push("Buzz");
-            else
-                results.push(i);
-       }
-       return results;
-    },
-    generateReverse: function(startAt, endAt){
-        if(!endAt){
-            endAt = startAt;
-            startAt = 1;
-        }
-       var results = []; 
-       for(var i = startAt; i <= endAt; i++){
-           if(isFizzBuzz(i))
-              results.push("FizzBuzz");
-           else if(isFizz(i))
-                results.push("Fizz");
-            else if(isBuzz(i))
-                results.push("Buzz");
-            else
-                results.push(i);
-       }
-       return results.reverse();
-    }
+    generate: generate,
+    generateReverse: generateReverse
 };
 
 function isFizz(input){
@@ -47,4 +13,31 @@ function isBuzz(input){
 
 function isFizzBuzz(input){
     return isFizz(input) && isBuzz(input);
+}
+
+function generate(startAt, endAt){
+    if(!endAt){
+        endAt = startAt;
+        startAt = 1;
+    }
+   var results = []; 
+   for(var i = startAt; i <= endAt; i++){
+       addItem(results, i);
+   }
+   return results;
+}
+
+function generateReverse(startAt, endAt){
+   return generate(startAt, endAt).reverse(); 
+}
+
+function addItem(arr, input ){
+   if(isFizzBuzz(input))
+      arr.push("FizzBuzz");
+   else if(isFizz(input))
+        arr.push("Fizz");
+    else if(isBuzz(input))
+        arr.push("Buzz");
+    else
+        arr.push(input);
 }
