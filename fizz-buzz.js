42a7d4236a71398881dca641c8af212de63a4b82
Eric Katz <ericpkatz@gmail.com>
2015-06-18 00:44:14 +0000

    get fizz buzz to work

--- fizz-buzz.js
+++ fizz-buzz.js
@@ -1,5 +1,24 @@
+function isFizz(input){
+    return input % 3 == 0;
+}
+
 module.exports = {
-    generate: function(){
-        return [1, 2, 'Fizz', 4, 'Buzz'];
+    generate: function(startAt, endAt){
+        if(!endAt){
+            endAt = startAt;
+            startAt = 1;
+        }
+       var results = []; 
+       for(var i = startAt; i <= endAt; i++){
+           if( i % 3 == 0 && i % 5 == 0)
+              results.push("FizzBuzz");
+           else if(isFizz(i))
+                results.push("Fizz");
+            else if(i % 5 == 0)
+                results.push("Buzz");
+            else
+                results.push(i);
+       }
+       return results;
     }
 };
--- spec/fizz_buzz_spec.js
+++ spec/fizz_buzz_spec.js
@@ -1,3 +1,4 @@
+/* globals describe it beforeEach expect */
 var FizzBuzz = require("../fizz-buzz");
 describe("FizzBuzz", function(){
     it("exists", function(){
@@ -12,6 +13,26 @@ describe("FizzBuzz", function(){
             
             it("returns [1,2,'Fizz', 4, 'Buzz']", function(){
                expect(results).toEqual([1,2,'Fizz', 4, 'Buzz']); 
+            });
+        });
+        describe("passing in 3", function(){
+            var results;
+            beforeEach(function(){
+                results = FizzBuzz.generate(3);
+            });
+            
+            it("returns [1,2,'Fizz']", function(){
+               expect(results).toEqual([1,2,'Fizz']); 
+            });
+        });
+        describe("passing in 14,15", function(){
+            var results;
+            beforeEach(function(){
+                results = FizzBuzz.generate(14, 15);
+            });
+            
+            it("returns [14, 'FizzBuzz']", function(){
+               expect(results).toEqual([14, 'FizzBuzz']); 
             });
         });
     });
